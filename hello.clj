b84b1f6da2f822a023f6f46aefd92c96d6a80037
ben <ben@localhost>
2014-08-16 10:53:17 -0400

    test2

---
+++ hello.clj
@@ -0,0 +1,11 @@
+;comment is beginning from a ;
+;Clojure's code is formed by a "form",( )
+;Clojure treated the first element in the form be a function or Macro, others
+;will treated be args
+;Clojure's first code statement is always using ns to specifiy current namespace
+(ns learnclojure)
+
+
+;more basic sampel
+
+(str "Hello" " " "World")
